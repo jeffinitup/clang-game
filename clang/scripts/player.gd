abd11f40c6892d6382c4cb9b35f2aaa06d15ea28
jeffinitup <48529067+jeffinitup@users.noreply.github.com>
2024-12-21 21:42:13 -0500

    add ze bounce

--- clang/scripts/player.gd
+++ clang/scripts/player.gd
@@ -3,7 +3,7 @@ class_name Player extends Entity
 
 ### Player specific functions
 
-@onready var player_controller := get_node("%controller") as Controller
+@onready var controller := get_node("%controller") as Controller
 @onready var player_weapon := get_node("player_weapon") as PlayerWeapon
 @onready var hbox := get_node("hbox") as Hitbox
 
--- clang/scripts/player_weapon.gd
+++ clang/scripts/player_weapon.gd
@@ -1,8 +1,17 @@
 ### player_weapon.gd
 class_name PlayerWeapon extends Node2D
 
+## Force to knock player back with
+const FORCE : float = 200.0
+
 @onready var player := owner as Player
 @onready var dbox := get_node("dbox") as Damagebox
+@onready var surface := get_node("surface") as RayCast2D
+
+func get_fling_vector() -> Vector2:
+	# Get angle of weapon
+	var rot := wrapf(rotation + PI, -PI, PI)
+	return Vector2.RIGHT.rotated(rot) * FORCE
 
 func update_rotation() -> void:
 	var mouse_pos : Vector2i = player.get_local_mouse_position()
