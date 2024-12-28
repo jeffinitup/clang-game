7421860c2f6c37dc5407ba3bc79121649223f67a
jeffinitup <48529067+jeffinitup@users.noreply.github.com>
2024-12-28 14:33:39 -0500

    controller support

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -5,18 +5,28 @@ const EXTENTS : float = 64.0
 
 @onready var origin : Vector2 = position
 var mouse_position : Vector2 = Vector2.ZERO
-var last_vel : Vector2 = Vector2.ZERO
+var last_joy : Vector2 = Vector2.ZERO
+var has_controller : bool = false
+
+func _ready() -> void:
+	if Input.get_connected_joypads().size() > 0:
+		has_controller = true
+	Input.joy_connection_changed.connect(
+		func(device : int, connected : bool):
+			has_controller = connected if device == 0 else has_controller
+	)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is not InputEventMouseMotion:
-		return
-	
-	owner = owner as Node2D
-	event = event as InputEventMouseMotion
-	
-	var mouse_speed : Vector2 = event.screen_relative
-	mouse_position += mouse_speed * 0.125
-	mouse_position = mouse_position.limit_length(EXTENTS)
-	position = origin + mouse_position
-	
-	last_vel = -owner.get_local_mouse_position().limit_length(EXTENTS)
+	if event is InputEventMouseMotion:
+		var mouse_speed : Vector2 = event.screen_relative
+		mouse_position += mouse_speed * 0.125
+		mouse_position = mouse_position.limit_length(EXTENTS)
+		position = origin + mouse_position
+
+func _process(delta: float) -> void:
+	if has_controller:
+		var axis := Input.get_vector("weapon_left", "weapon_right", "weapon_up", "weapon_down").normalized()
+		axis *= EXTENTS
+		if axis.length() == EXTENTS:
+			position = origin + axis 
+		
--- scripts/titlescreen.gd
+++ scripts/titlescreen.gd
@@ -9,9 +9,9 @@ extends Node
 ]
 
 func _process(delta: float) -> void:
-	var time := Time.get_ticks_usec() / 1000.0
+	var time := Time.get_ticks_usec() / 100000.0
 	var pos : int = 0
 	for effect in title_effect:
-		effect.position.x = sin(time * delta / (8 if pos != 1 else 4)) * 10
-		effect.position.y = sin(time * delta / (6 if pos != 1 else 8)) * 12
+		effect.position.x = sin(time / (7.25 if pos != 1 else -4.5)) * 8
+		effect.position.y = sin(time / (-5.5 if pos != 1 else 7.5)) * 8
 		pos += 1
